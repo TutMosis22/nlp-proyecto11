70bbfc04dc2064141a4ba5e8a06c888c3b45b47f
TutMosis22 <jairo.calagua.m@uni.pe>
2025-04-23 08:15:25 -0500

    pipeline modular de IA generativa

---
+++ src/pipeline.py
@@ -0,0 +1,50 @@
+from diffusers import StableDiffusionPipeline
+import torch
+from PIL import Image
+from IPython.display import display
+
+def load_model(model_name = "runwayml/stable-diffusion-v1-5"):
+    """
+    Carga el modelo de difusión estable desde Hugging Face y lo envía a la GPU si está disponible
+    
+    Parámetros:
+    - model_name (str): Nombre del modelo en Hugging Face
+    
+    Retorna:
+    - pipe: pipeline ya cargado y listo para usar
+    """
+    
+    pipe = StableDiffusionPipeline.from_pretrained(model_name)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    pipe = pipe.to(device)
+    
+    return pipe
+
+def generate_image(pipe, prompt):
+    """
+    Usa el modelo para generar una imagen a partir de un texto
+    
+    Parámetros:
+    - pipe: El pipeline de difusión cargado
+    - prompt (str): El texto que describe la imagen
+    
+    Retorna:
+    - image(PIL.Image): La imagen ya generada
+    """
+    image = pipe(prompt).images[0]
+    
+    return image
+
+def show_image(image):
+    """
+    Se muestra la imagen generada.
+    
+    Parámetros:
+    - image (PIL.Image): Imagen a mostrar
+    """
+    
+    try:
+        display(image)
+    except:
+        image.show()
+    
