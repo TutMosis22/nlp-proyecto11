1ed2be2e3c9698155b2d971eaedd10889306c157
TutMosis22 <jairo.calagua.m@uni.pe>
2025-05-01 21:25:48 -0500

    Imagen estilizada

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.my_pipeline import image_to_image, show_image, save_image
+from src.my_pipeline import image_to_image, show_image, save_image, stylize_image
 
 prompt = "A man in profile with straight hair and a jacket"
 init_image_path = "assets/dibujo_mio.png"  # Imagen base (debe existir)
@@ -6,3 +6,6 @@ init_image_path = "assets/dibujo_mio.png"  # Imagen base (debe existir)
 result_image = image_to_image(prompt, init_image_path)
 show_image(result_image)
 save_image(result_image, "metrics/man_jacket.png")
+
+original_path = "assets/dibujo_mio.png"
+stylized_image = stylize_image(original_path)
