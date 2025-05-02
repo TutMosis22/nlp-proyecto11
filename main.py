cb76ef395634bbc99266614ed0578c16b760413a
TutMosis22 <jairo.calagua.m@uni.pe>
2025-05-01 21:41:51 -0500

    Cambio para mascara

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.my_pipeline import image_to_image, show_image, save_image, stylize_image
+from src.my_pipeline import image_to_image, show_image, save_image, stylize_image, inpaint_image
 
 prompt = "A man in profile with straight hair and a jacket"
 init_image_path = "assets/dibujo_mio.png"  # Imagen base (debe existir)
@@ -9,3 +9,8 @@ save_image(result_image, "metrics/man_jacket.png")
 
 original_path = "assets/dibujo_mio.png"
 stylized_image = stylize_image(original_path)
+
+prompt = "una version futurista del dibujo original",
+image_path="assets/dibujo_mio.png"
+mask_path = "assets/mi_dibujo_mascara.png",
+save_path = "metrics/dibujo_pintado.png"
